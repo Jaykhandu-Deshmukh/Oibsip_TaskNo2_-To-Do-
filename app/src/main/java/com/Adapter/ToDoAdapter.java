7c822638041fedd98b87c79b8fb4a344dc1e6d76
Jaykhandu Deshmukh <jaykhandudeshmukh7777@gmail.com>
2023-10-18 20:58:14 +0530

    finish

---
+++ app/src/main/java/com/Adapter/ToDoAdapter.java
@@ -0,0 +1,106 @@
+package com.Adapter;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.AddNewTask;
+import com.Model.ToDoModel;
+import com.Utils.DataBaseHelper;
+import com.example.to_do.MainActivity;
+import com.example.to_do.R;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.MyViewHolder> {
+    private List<ToDoModel> mList;
+    private MainActivity activity;
+    private DataBaseHelper myDB;
+
+    public ToDoAdapter(DataBaseHelper myDB, MainActivity activity){
+        this.activity=activity;
+        this.myDB=myDB;
+
+    }
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.task_layout, parent, false);
+        return  new MyViewHolder(v);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        final ToDoModel item=mList.get(position);
+        holder.mCheckBox.setText(item.getTask());
+        holder.mCheckBox.setChecked(toBoolean(item.getStatus()));
+        holder.mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    myDB.updateStatus(item.getId(), 1);
+                }else{
+                    myDB.updateStatus(item.getId(), 0);
+                }
+            }
+        });
+    }
+
+    public boolean toBoolean(int num){
+        return num!=0;
+    }
+
+    public Context getContext(){
+        return activity;
+    }
+
+    public void setTask(List<ToDoModel> mList){
+        this.mList= mList;
+        notifyDataSetChanged();
+    }
+    public void deleteTask(int position){
+        ToDoModel item= mList.get(position);
+        myDB.deleteTask(item.getId());
+        mList.remove(position);
+        notifyItemRemoved(position);
+
+    }
+
+
+    public void editItem(int position){
+        ToDoModel item= mList.get(position);
+
+        Bundle bundle =new Bundle();
+        bundle.putInt("Id", item.getId());
+        bundle.putString("task", item.getTask());
+
+        AddNewTask task=new AddNewTask();
+        task.setArguments(bundle);
+        task.show(activity.getSupportFragmentManager(), task.getTag());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        CheckBox mCheckBox;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mCheckBox= itemView.findViewById(R.id.mcheckbox);
+        }
+    }
+}
